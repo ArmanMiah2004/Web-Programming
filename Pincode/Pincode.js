87e93ad3c57c59ecb398b9e98bdd1aa6b2c01915
ArmanMiah2004 <ittihad720@gmail.com>
2023-09-23 00:55:18 +0100

    I made it so it will save the username and pin code locally on the user's computer.

--- Pincode/Pincode.js
+++ Pincode/Pincode.js
@@ -1,23 +1,54 @@
-// Define the correct PIN code
-const correctPIN = "1234"; // Change this to your desired PIN
-
 // Get form and message elements
 const pinForm = document.getElementById("pinForm");
+const usernameInput = document.getElementById("usernameInput");
 const pinInput = document.getElementById("pinInput");
+const registrationModeInput = document.getElementById("registrationMode");
 const message = document.getElementById("message");
+
+// Function to store user data in local storage
+function storeUserData(username, pin) {
+    const userData = JSON.parse(localStorage.getItem('userData')) || {};
+    userData[username] = pin;
+    localStorage.setItem('userData', JSON.stringify(userData));
+}
+
+// Function to retrieve user data from local storage
+function retrieveUserData() {
+    return JSON.parse(localStorage.getItem('userData')) || {};
+}
 
 // Add an event listener to the form for PIN submission
 pinForm.addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent the form from submitting normally
 
+    const username = usernameInput.value;
     const enteredPIN = pinInput.value;
+    const registrationMode = registrationModeInput.value === "true";
 
-    if (enteredPIN === correctPIN) {
-        // Redirect to your main website page or unlock the content
-        window.location.href = "main.html"; // Replace "main.html" with your actual main page URL
+    if (registrationMode) {
+        // Registration mode: Create a new username and PIN
+        const userData = retrieveUserData();
+
+        if (userData[username]) {
+            message.textContent = "Username already exists. Please enter your existing PIN.";
+            pinInput.value = ""; // Clear the input field
+        } else {
+            storeUserData(username, enteredPIN);
+            message.textContent = "Registration successful! You can now use your new PIN to log in.";
+            pinInput.value = ""; // Clear the input field
+            registrationModeInput.value = "false"; // Switch back to login mode
+        }
     } else {
-        // Display an error message
-        message.textContent = "Incorrect PIN. Please try again.";
-        pinInput.value = ""; // Clear the input field
+        // Login mode: Check if the username and PIN match
+        const userData = retrieveUserData();
+        const storedPIN = userData[username];
+
+        if (storedPIN === enteredPIN) {
+            // Redirect to your main website page or unlock the content
+            window.location.href = "main.html"; // Replace "main.html" with your actual main page URL
+        } else {
+            message.textContent = "Incorrect username or PIN. Please try again.";
+            pinInput.value = ""; // Clear the input field
+        }
     }
 });
